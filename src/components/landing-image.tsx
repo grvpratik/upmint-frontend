26b3df77e12c26492d7d84754d93ad61c49a3e9f
pratikgrv <pratikgaurav37@gmail.com>
2024-05-19 13:26:56 +0530

    ui :art: : theme changed back to original

--- src/components/landing-image.tsx
+++ src/components/landing-image.tsx
@@ -6,42 +6,42 @@ const LandingImages = () => {
 		<div className=" -z-40 absolute inset-0  overflow-hidden hidden lg:block">
 			<div className="  rounded-3xl overflow-hidden h-28 w-28 aspect-square bg-lime-500 absolute right-0 bottom-0">
 				<div className=" relative w-full h-full ">
-					<Image src={"/images/1.jpg"} fill alt="" />
+					<Image src={"/images/1.jpg"} fill alt="landing images for upmint" />
 				</div>
 			</div>
 			<div className="  rounded-3xl overflow-hidden h-28 w-28 bg-blue-500 absolute right-[14rem] -bottom-[1rem]">
 				<div className=" relative w-full h-full ">
-					<Image src={"/images/2.jpg"} fill alt="" />
+					<Image src={"/images/2.jpg"} fill alt="landing images for upmint" />
 				</div>
 			</div>
 			<div className="  rounded-3xl overflow-hidden h-28 w-28 bg-orange-500  absolute right-[28rem] -bottom-[2rem]">
 				<div className=" relative w-full h-full ">
-					<Image src={"/images/3.jpg"} fill alt="" />
+					<Image src={"/images/3.jpg"} fill alt="landing images for upmint" />
 				</div>
 			</div>
 			<div className="  rounded-3xl overflow-hidden h-28 w-28 bg-orange-500  absolute -right-[1rem] bottom-[10rem]">
 				<div className=" relative w-full h-full ">
-					<Image src={"/images/4.jpg"} fill alt="" />
+					<Image src={"/images/4.jpg"} fill alt="landing images for upmint" />
 				</div>
 			</div>
 			<div className="  rounded-3xl overflow-hidden h-28 w-28 bg-orange-500  absolute right-[9rem] bottom-[9rem]">
 				<div className=" relative w-full h-full ">
-					<Image src={"/images/5.jpg"} fill alt="" />
+					<Image src={"/images/5.jpg"} fill alt="landing images for upmint" />
 				</div>
 			</div>
 			<div className="  rounded-3xl overflow-hidden h-28 w-28 bg-blue-500 absolute right-[19rem] bottom-[8rem]">
 				<div className=" relative w-full h-full ">
-					<Image src={"/images/6.jpg"} fill alt="" />
+					<Image src={"/images/6.jpg"} fill alt="landing images for upmint" />
 				</div>
 			</div>
 			<div className="  rounded-3xl overflow-hidden h-28 w-28 bg-orange-500  absolute -right-[2rem] bottom-[20rem]">
 				<div className=" relative w-full h-full ">
-					<Image src={"/images/7.jpg"} fill alt="" />
+					<Image src={"/images/7.jpg"} fill alt="landing images for upmint" />
 				</div>
 			</div>
 			<div className="  rounded-3xl overflow-hidden h-28 w-28 bg-blue-500 absolute right-[8rem] bottom-[19rem]">
 				<div className=" relative w-full h-full ">
-					<Image src={"/images/9.jpg"} fill alt="" />
+					<Image src={"/images/9.jpg"} fill alt="landing images for upmint" />
 				</div>
 			</div>
 		</div>
